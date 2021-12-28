10d6f58ee6cae9f2900a1b124260b8e93762ed26
Mohammad Abusaa <mohmdsalam94@gmail.com>
2021-12-28 12:06:49 +0200

    Step 1

--- CalculatorService.Tests/UnitTest1.fs
+++ CalculatorService.Tests/UnitTest1.fs
@@ -1,11 +1,38 @@
 module CalculatorService.Tests
 
 open NUnit.Framework
-
-[<SetUp>]
-let Setup () =
-    ()
+open CalculatorService
 
 [<Test>]
-let Test1 () =
-    Assert.Pass()
+let ``Test empty strings to return 0`` () =
+    let expected = 0
+    let actual = Calculator.add("")
+    Assert.That(actual, Is.EqualTo(expected))
+
+[<Test>]
+let ``Test one number string to return the same integer value`` () =
+    let mutable expected = 25
+    let mutable actual = Calculator.add("25")
+    Assert.That(actual, Is.EqualTo(expected))
+    expected <- -16
+    actual <- Calculator.add "-16"
+    Assert.That(actual, Is.EqualTo(expected))
+    expected <- -255
+    actual <- Calculator.add "-255"
+    Assert.That(actual, Is.EqualTo(expected))
+
+
+[<Test>]
+let ``Test two number string to return the integer value of their summation`` () =
+    let mutable expected = 13
+    let mutable actual = Calculator.add("7,6")
+    Assert.That(actual, Is.EqualTo(expected))
+    expected <- -10
+    actual <- Calculator.add "0,-10"
+    Assert.That(actual, Is.EqualTo(expected))
+    expected <- 20
+    actual <- Calculator.add "9,11"
+    Assert.That(actual, Is.EqualTo(expected))
+    expected <- 0
+    actual <- Calculator.add "100,-100"
+    Assert.That(actual, Is.EqualTo(expected))
--- CalculatorService/Library.fs
+++ CalculatorService/Library.fs
@@ -1,5 +1,15 @@
 ﻿namespace CalculatorService
 
-module Say =
-    let hello name =
-        printfn "Hello %s" name
+open System
+
+module Calculator =
+    let add (str: string) : int =
+        if String.IsNullOrEmpty str then
+            0
+        else if String.exists (fun char -> char = ',') str then
+            let index = str.IndexOf ','
+            let firstNumber = str.[0..index - 1]
+            let secondNumber = str.[index + 1..]
+            int (firstNumber) + int (secondNumber)
+        else
+            int (str)
