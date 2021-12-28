b01334fae903a046e1e511e6d4509e1fcc3a0137
Mohammad Abusaa <mohmdsalam94@gmail.com>
2021-12-28 14:38:30 +0200

    Step 5, and fixed previuos tests because they contained negatives

--- CalculatorService.Tests/UnitTest1.fs
+++ CalculatorService.Tests/UnitTest1.fs
@@ -14,11 +14,11 @@ let ``Test one number string to return the same integer value`` () =
     let mutable expected = 25
     let mutable actual = Calculator.add("25")
     Assert.That(actual, Is.EqualTo(expected))
-    expected <- -16
-    actual <- Calculator.add "-16"
+    expected <- 16
+    actual <- Calculator.add "16"
     Assert.That(actual, Is.EqualTo(expected))
-    expected <- -255
-    actual <- Calculator.add "-255"
+    expected <- 255
+    actual <- Calculator.add "255"
     Assert.That(actual, Is.EqualTo(expected))
 
 
@@ -27,54 +27,47 @@ let ``Test two number string to return the integer value of their summation`` ()
     let mutable expected = 13
     let mutable actual = Calculator.add("7,6")
     Assert.That(actual, Is.EqualTo(expected))
-    expected <- -10
-    actual <- Calculator.add "0,-10"
+    expected <- 10
+    actual <- Calculator.add "0,10"
     Assert.That(actual, Is.EqualTo(expected))
     expected <- 20
     actual <- Calculator.add "9,11"
     Assert.That(actual, Is.EqualTo(expected))
-    expected <- 0
-    actual <- Calculator.add "100,-100"
+    expected <- 200
+    actual <- Calculator.add "100,100"
     Assert.That(actual, Is.EqualTo(expected))
 
 [<Test>]
 let ``Test more than two numbers to return the integer value of their summation`` () =
-    let mutable expected = 90
-    let mutable actual = Calculator.add "10,20,30,40,50,-60"
+    let mutable expected = 210
+    let mutable actual = Calculator.add "10,20,30,40,50,60"
     Assert.That(actual, Is.EqualTo(expected))
-    expected <- -300
-    actual <- Calculator.add "-10,-20,-70,-100,-200,100"
+    expected <- 300
+    actual <- Calculator.add "10,20,70,100,100"
     Assert.That(actual, Is.EqualTo(expected))
 
 [<Test>]
 let ``Test newline as another separator`` () =
-    let mutable expected = 66
-    let mutable actual = Calculator.add "1
-    2
-    500,-400
-    -37"
+    let mutable expected = 940
+    let mutable actual = Calculator.add "1\n2\n500,400\n37"
     Assert.That(actual, Is.EqualTo(expected))
-    expected <- -10
-    actual <- Calculator.add "5,-10
-    -1
-    -2
-    -3
-    1"
+    expected <- 22
+    actual <- Calculator.add "5,10\n1\n2\n3\n1"
     Assert.That(actual, Is.EqualTo(expected))
 
 [<Test>]
 let ``Test different delimiters to be supported`` () =
     let mutable expected = 15
-    let mutable actual = Calculator.add "//;
-    1;2;3;4;5"
+    let mutable actual = Calculator.add "//;\n1;2;3;4;5"
     Assert.That(actual, Is.EqualTo(expected))
-    expected <- -20
-    actual <- Calculator.add "//d
-    -2d-4d-6d-8"
+    expected <- 20
+    actual <- Calculator.add "//d\n2d4d6d8"
     Assert.That(actual, Is.EqualTo(expected))
-    expected <- 0
-    actual <- Calculator.add "1,3
-    5
-    7
-    9,-25"
+    expected <- 50
+    actual <- Calculator.add "1,3\n5\n7\n9,25"
     Assert.That(actual, Is.EqualTo(expected))
+
+[<Test>]
+let ``Test throwing an exception when sending negative numbers`` () =
+    Assert.Throws<System.FormatException>((fun _ -> Calculator.add "1,2,-3,-4,-5,-6" |> ignore), "Negatives are not allowed! the negatives are:\n-3,-4,-5,-6") |> ignore
+    Assert.Throws<System.FormatException>((fun _ -> Calculator.add "//$\n1$2$-39$-40$-53$-26" |> ignore), "Negatives are not allowed! the negatives are:\n-39,-40,-53,-26") |> ignore
--- CalculatorService/Library.fs
+++ CalculatorService/Library.fs
@@ -3,13 +3,22 @@
 open System
 
 module Calculator =
+    let mutable negatives = []
+    let comparator (i : string) =
+        if int(i) < 0 then
+            negatives <- i :: negatives
+        int(i)
     let reducer (d : char[]) (s : string) =
-        s.Split(d) |> Array.map (fun c -> int(c)) |> Array.reduce (fun a b -> a + b)
+        let ints = s.Split(d) |> Array.map (comparator)
+        if negatives.Length > 0 then
+            raise (FormatException(("Negatives are not allowed! the negatives are:\n" + String.concat "," (negatives |> List.rev))))
+        else ints |> Array.reduce (fun a b -> a + b)
     let add (str: string) : int =
+        negatives <- List.empty
         if String.IsNullOrEmpty str then
             0
         else if str.StartsWith("//") then
-            reducer [|str.[2]|] str.[3..]
+            reducer [|str.[2]|] str.[4..]
         else if String.exists (fun c -> c = ',' || c = '\n') str then
             reducer [|','; '\n'|] str
         else
